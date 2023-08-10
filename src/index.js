2434086a5fbbc8ad0bb29e77d44888034a5e7fd7
etiry <7997357+etiry@users.noreply.github.com>
2023-08-10 13:05:14 -0400

    Add render result function

--- src/index.js
+++ src/index.js
@@ -38,15 +38,32 @@ const Food = (data) => {
   }
 }
 
-const fetchFood = function (query) {
+const setFood = (data) => {
+  return Food(data);
+}
+
+const fetchFood = (query) => {
   const url = `https://api.edamam.com/api/food-database/v2/parser?app_id=${config.configInfo.appId}&app_key=${config.configInfo.appKey}&ingr=${query}`;
 
   fetch(url, {
     method: 'GET',
     dataType: 'json'
   })
-    .then(data => data.json())
-    .then(data => console.log(Food(data)))
+    .then(response => response.json())
+    .then(data => setFood(data))
+    .then(result => renderResult(result));
 };
+
+const renderResult = (food) => {
+  const result = document.querySelector('.result');
+
+  result.replaceChildren();
+
+  const template = `
+    <h1> ${ food.food.isProteinFood } </h1>
+  `;
+
+  result.innerHTML = template;
+}
 
 fetchFood('Parmesan');
