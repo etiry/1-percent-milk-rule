17a2dc97a923bb85f35fcdce57777e3c5c45db9b
etiry <7997357+etiry@users.noreply.github.com>
2023-08-10 10:48:21 -0400

    Create food object from API result

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 import config from '../config';
 
+const Food = (data) => {
+
+  const getFood = (data) => {
+    if (data.parsed) {
+      return data.parsed[0].food; 
+    } else {
+      return data.parsed[0].food;
+    }
+  }
+
+  const calculateProteinRatio = (food) => {
+    const onePercentMilkRatio = (3.37 * 4) / 42;
+    const foodRatio = (food.nutrients.PROCNT * 4) / food.nutrients.ENERC_KCAL;  
+
+    if (foodRatio >= onePercentMilkRatio) {
+      return true;
+    }
+
+    return false;
+  }
+
+  const getFoodDetails = (food) => {
+    return {
+      id: food.foodId,
+      name: food.label,
+      calories: food.nutrients.ENERC_KCAL,
+      protein: food.nutrients.PROCNT,
+      isProteinFood: calculateProteinRatio(food)
+    }
+  }
+
+  const food = getFoodDetails(getFood(data));
+
+  return {
+    food
+  }
+}
+
 const fetchFood = function (query) {
   const url = `https://api.edamam.com/api/food-database/v2/parser?app_id=${config.configInfo.appId}&app_key=${config.configInfo.appKey}&ingr=${query}`;
 
@@ -8,7 +46,7 @@ const fetchFood = function (query) {
     dataType: 'json'
   })
     .then(data => data.json())
-    .then(data => console.log(data))
+    .then(data => console.log(Food(data)))
 };
 
-fetchFood('broccoli');
+fetchFood('Parmesan');
