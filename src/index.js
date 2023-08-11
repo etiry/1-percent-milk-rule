e72c65cdf79e9cefa95e118f4f0e9c724bb99ce7
etiry <7997357+etiry@users.noreply.github.com>
2023-08-11 10:04:57 -0400

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,58 +1,40 @@
 import config from '../config';
 
 const Food = (data) => {
-
-  const getFood = (data) => {
-    if (data.parsed) {
-      return data.parsed[0].food; 
-    } else {
-      return data.parsed[0].food;
+  const getFood = (food) => {
+    if (!food.parsed) {
+      return food.hints[0].food;
     }
-  }
+    return food.parsed[0].food;
+  };
 
   const calculateProteinRatio = (food) => {
     const onePercentMilkRatio = (3.37 * 4) / 42;
-    const foodRatio = (food.nutrients.PROCNT * 4) / food.nutrients.ENERC_KCAL;  
+    const foodRatio = (food.nutrients.PROCNT * 4) / food.nutrients.ENERC_KCAL;
 
     if (foodRatio >= onePercentMilkRatio) {
       return true;
     }
 
     return false;
-  }
+  };
 
-  const getFoodDetails = (food) => {
-    return {
-      id: food.foodId,
-      name: food.label,
-      calories: food.nutrients.ENERC_KCAL,
-      protein: food.nutrients.PROCNT,
-      isProteinFood: calculateProteinRatio(food)
-    }
-  }
+  const getFoodDetails = (food) => ({
+    id: food.foodId,
+    name: food.label,
+    calories: food.nutrients.ENERC_KCAL,
+    protein: food.nutrients.PROCNT,
+    isProteinFood: calculateProteinRatio(food),
+  });
 
   const food = getFoodDetails(getFood(data));
 
   return {
-    food
-  }
-}
-
-const setFood = (data) => {
-  return Food(data);
-}
-
-const fetchFood = (query) => {
-  const url = `https://api.edamam.com/api/food-database/v2/parser?app_id=${config.configInfo.appId}&app_key=${config.configInfo.appKey}&ingr=${query}`;
-
-  fetch(url, {
-    method: 'GET',
-    dataType: 'json'
-  })
-    .then(response => response.json())
-    .then(data => setFood(data))
-    .then(result => renderResult(result));
+    food,
+  };
 };
+
+const setFood = (data) => Food(data);
 
 const renderResult = (food) => {
   const result = document.querySelector('.result');
@@ -60,10 +42,21 @@ const renderResult = (food) => {
   result.replaceChildren();
 
   const template = `
-    <h1> ${ food.food.isProteinFood } </h1>
-  `;
+    <h1> ${food.food.isProteinFood} </h1>`;
 
   result.innerHTML = template;
-}
+};
+
+const fetchFood = (query) => {
+  const url = `https://api.edamam.com/api/food-database/v2/parser?app_id=${config.configInfo.appId}&app_key=${config.configInfo.appKey}&ingr=${query}`;
+
+  fetch(url, {
+    method: 'GET',
+    dataType: 'json',
+  })
+    .then((response) => response.json())
+    .then((data) => setFood(data))
+    .then((result) => renderResult(result));
+};
 
 fetchFood('Parmesan');
